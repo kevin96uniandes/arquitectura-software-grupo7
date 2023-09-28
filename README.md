5e355e8f45c179824548465cb59f1874a0748bd3
Kevin Alexander Maldonado Delgado <k.maldonadod@uniandes.edu.co>
2023-09-28 11:35:25 -0500

    componente candidato con autenticacion

--- README.md
+++
@@ -1,6 +0,0 @@
-# Prerequisitos
-Docker compose
-# Como ejecutarlo
-1. Descargue el repositorio
-2. Dirijase al directorio _"arquitectura-software-grupo7"_
-3. Abra una terminal desde el directorio anterior, e ingrese el comando _docker-compose up -d
--- candidatos/app.py
+++ candidatos/app.py
@@ -1,16 +1,23 @@
-from flask import Flask
-import requests
+from flask import Flask, jsonify, request
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default_secret')
+app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY', 'default_jwt_secret')
 
-@app.route('/crear-candidato', methods=['POST'])
-def crearCandidato():
-    return 'Candidato creado!', 200
-    
-@app.route('/healthcheck')
-def healthcheck():
-    response = requests.get("http://mockserver:8080/mock3/health")
-    return '', response.status_code
+
+jwt = JWTManager(app)
+
+
+@app.route('/apply-to-job', methods=['POST'])
+@jwt_required()
+def applyToJob():
+    claims = get_jwt()
+    if claims['role'] != 'Candidato':
+        return jsonify({"message": "Access forbidden: Candidatos only"}), 403
+    return jsonify({"message": "Application registered!"})
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
